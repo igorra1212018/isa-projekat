26c21f19ad1143e62da8c748c4db23c18b0f6561
FedorBozicRA99-2018 <74472316+FedorBozicRA99-2018@users.noreply.github.com>
2021-12-18 23:24:35 +0100

    feat: Basic frontend-backend integration

--- fishing-app/src/main/java/com/isa/fishingapp/controller/UserController.java
+++ fishing-app/src/main/java/com/isa/fishingapp/controller/UserController.java
@@ -1,20 +1,38 @@
 package com.isa.fishingapp.controller;
 
+import java.awt.SystemColor;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.isa.fishingapp.model.User;
 import com.isa.fishingapp.service.UserService;
 
-@Controller
+@RestController
+@RequestMapping("/api")
+@CrossOrigin("http://localhost:4000/")
 public class UserController {
 	
 	@Autowired
 	UserService userService;
+	
+	@GetMapping("/users")
+	public List<User> getUsers(Model model)
+	{
+		List<User> users = userService.getAllUsers();
+		for(User u : users)
+		{
+			System.out.println(u.getEmail());
+		}
+		return userService.getAllUsers();
+	}
 	
 	@GetMapping("/register")
 	public String getRegisterPage(Model model)
--- fishing-app/src/main/java/com/isa/fishingapp/service/UserService.java
+++ fishing-app/src/main/java/com/isa/fishingapp/service/UserService.java
@@ -1,5 +1,7 @@
 package com.isa.fishingapp.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -27,5 +29,10 @@ public class UserService {
 	public User authenticate(String email, String password)
 	{
 		return userRepository.findByEmailAndPassword(email, password).orElse(null);
+	}
+	
+	public List<User> getAllUsers()
+	{
+		return userRepository.findAll();
 	}
 }
